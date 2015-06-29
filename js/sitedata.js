4c6f7f60f0f456a0031b05e2155728696b8971cd
Vasily Myazin <vm@konspiredesign.com>
2015-06-28 23:01:15 -0400

    Adds episode #10

--- js/sitedata.js
+++ js/sitedata.js
@@ -5,8 +5,9 @@ siteData.episodes = [
   {id: 3, title: 'Ведущие встретились в Калифорнии поговорить об Apple Watch.', file: 'Episodes/Techlife-2015-03-29.mp3', dateLang: '29 марта 2015'},
   {id: 4, title: 'Музыкальные стриминг-сервисы, батареи и родные приложения для смартрфонов.', file: 'Episodes/Techlife-2015-04-05.mp3', dateLang: '5 апреля 2015'},
   {id: 5, title: 'Про монополию Гугла в рекламе и снова про Apple Watch.', file: 'Episodes/Techlife-2015-04-19.mp3', dateLang: '19 апреля 2015'},
-  {id: 6, title: 'Новый Макбук Про и сила нажатия. Первые впечатления о часах Эппл', file: 'Episodes/Techlife-2015-05-01.mp3', dateLang: '1 мая 2015'},
-  {id: 7, title: 'Диктовка в телефон и реклама в онлайне', file: 'Episodes/Techlife-2015-05-25.mp3', dateLang: '25 мая 2015'},
+  {id: 6, title: 'Новый Макбук Про и сила нажатия. Первые впечатления о часах Эппл.', file: 'Episodes/Techlife-2015-05-01.mp3', dateLang: '1 мая 2015'},
+  {id: 7, title: 'Диктовка в телефон и реклама в онлайне.', file: 'Episodes/Techlife-2015-05-25.mp3', dateLang: '25 мая 2015'},
   {id: 8, title: 'Про искусственный интеллект в кино.', file: 'Episodes/Techlife-2015-05-31.mp3', dateLang: '31 мая 2015'},
   {id: 9, title: 'Цена приватности в сети.', file: 'Episodes/Techlife-2015-06-10.mp3', dateLang: '10 июня 2015'},
+  {id: 10, title: 'Скорбь по ушедшим сервисам и продление жизни.', file: 'Episodes/Techlife-2015-06-27.mp3', dateLang: '27 июня 2015'}
 ];
